847c6c21ea05ecbc76cdf47261d98803fbad28af
xshi88 <xiaonanshi@gatech.edu>
2024-06-06 16:16:09 -0700

    try to deploy

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PyPDF2 import PdfReader
+#from PyPDF2 import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
